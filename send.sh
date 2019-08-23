ca758744a862b8dbabe3abd54fb458becad513b7
ShchAlexander <shchalexander@gmail.com>
2019-08-23 12:21:37 +0300

    added artifact version column

--- send.sh
+++ send.sh
@@ -30,6 +30,8 @@ AUTHOR_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%aN")"
 COMMITTER_NAME="$(git log -1 "$TRAVIS_COMMIT" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$TRAVIS_COMMIT" --pretty="%s")"
 COMMIT_MESSAGE="$(git log -1 "$TRAVIS_COMMIT" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
+PRODUCED_VERSION = "$3"
+PRODUCED_ARTIFACT_URL = "$4"
 
 if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
   CREDITS="$AUTHOR_NAME authored & committed"
@@ -41,6 +43,16 @@ if [[ $TRAVIS_PULL_REQUEST != false ]]; then
   URL="https://github.com/$TRAVIS_REPO_SLUG/pull/$TRAVIS_PULL_REQUEST"
 else
   URL=""
+fi
+if [ "$PRODUCED_VERSION" == "" ]; then
+	PRODUCED_VERSION_DATA = ""
+else 
+	PRODUCED_VERSION_DATA = ',
+      {
+        "name": "Artifact",
+        "value": "'"[\`$PRODUCED_VERSION_DATA\`]($PRODUCED_ARTIFACT_URL)"'",
+        "inline": true
+      }'
 fi
 
 TIMESTAMP=$(date --utc +%FT%TZ)
@@ -67,7 +79,7 @@ WEBHOOK_DATA='{
         "name": "Branch",
         "value": "'"[\`$TRAVIS_BRANCH\`](https://github.com/$TRAVIS_REPO_SLUG/tree/$TRAVIS_BRANCH)"'",
         "inline": true
-      }
+      }'"$PRODUCED_VERSION_DATA"'
     ],
     "timestamp": "'"$TIMESTAMP"'"
   } ]
